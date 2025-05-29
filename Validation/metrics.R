2bdbe03e23b9f14340bb414b3654d22332193867
Xeri <79507960+Lasya-Pasumarthy@users.noreply.github.com>
2025-05-29 13:34:12 -0400

    Update metrics.R

--- Validation/metrics.R
+++ Validation/metrics.R
@@ -34,11 +34,11 @@ print(delta_r2)
 
 #----AUC + Plot----
 prob_base <- predict(model_base,type = "response")
-prob_adjusted - predict(model_adjusted, type = "response")
+prob_adjusted <- predict(model_adjusted, type = "response")
 
 #Computing ROC values
-roc_base <- roc(data$Status, model_base)
-roc_adjusted <- roc(data$Status, model_adjusted)
+roc_base <- roc(data$Status, prob_base)
+roc_adjusted <- roc(data$Status, prob_adjusted)
 
 #Plot
 plot(roc_base, col = "#1c61b6", main = "AUC Comparison", legacy.axes = TRUE,
@@ -46,13 +46,13 @@ plot(roc_base, col = "#1c61b6", main = "AUC Comparison", legacy.axes = TRUE,
 lines(roc_adjusted, col = "#e94e77")
 legend("bottomright", legend = c(
   paste("AUC (Cov only):", round(auc(roc_base), 2)),
-  paste("AUC (PRS + Cov):", round(auc(roc_cov), 2))
+  paste("AUC (PRS + Cov):", round(auc(roc_adjusted), 2))
 ), col = c("#1c61b6", "#e94e77"), lwd = 2)
 
 #----Odds Ratio + Confidence Interval----
 
-or <- exp(coef(model_adjusted)
-ci <- exp(confint(logit_model)
+or <- exp(coef(model_adjusted))
+ci <- exp(confint(model_adjusted))
 
 #View them as a dataframe
 stats <- data.frame(
